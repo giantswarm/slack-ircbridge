30a15206d3d9b0dafd07fe096e1e78c012fed5cb
marians <marian@giantswarm.io>
2017-05-11 13:44:45 +0200

    Using named environment variables instead of args

--- startup.sh
+++ startup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-set -e
-
 CONFIG_PATH=/tmp/config.json
-
-NICKNAME=$1
-SERVER=$2
-TOKEN=$3
-SLACK_CHANNEL=$4
-IRC_CHANNEL=$5
 
 TEMPLATE="[{\"nickname\":\"${NICKNAME}\",\"server\":\"${SERVER}\",\"token\":\"${TOKEN}\",\"channelMapping\":{\"#${SLACK_CHANNEL}\":\"#${IRC_CHANNEL}\"}}]"
 
