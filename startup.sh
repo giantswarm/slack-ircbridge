0a40662b7b37976bfbcd3977d2e3f62dab3fbf40
marians <marian@giantswarm.io>
2017-05-18 16:00:50 +0200

    Fix for alpine (no bash available)

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 CONFIG_PATH=/tmp/config.json
 
 TEMPLATE="[{\"nickname\":\"${NICKNAME}\",\"server\":\"${SERVER}\",\"token\":\"${TOKEN}\",\"channelMapping\":{\"#${SLACK_CHANNEL}\":\"#${IRC_CHANNEL}\"}}]"
 
 echo $TEMPLATE > $CONFIG_PATH
-exec slack-irc --config $CONFIG_PATH
+slack-irc --config $CONFIG_PATH
